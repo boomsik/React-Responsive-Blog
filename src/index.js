ac98abbd12080673e20fcac67fe538925d78425b
boomsik <pozdnyakov151994@gmail.com>
2024-01-19 04:35:05 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
-import CreatePost from "./pages/CreatePost";
+import { HashRouter } from "react-router-dom";
 
 import reportWebVitals from "./reportWebVitals";
 
